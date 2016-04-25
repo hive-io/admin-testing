bff3ebd17ad4fd2f57a2ecc6c1a25167867aa0ef
Olek <malahoff@gmail.com>
2016-04-25 17:10:46 -0400

    feat(template validation): Check path validation
    Check if invalid input generates input error for path.

---
+++ tests/publishing/templates/validations.test.js
@@ -0,0 +1,51 @@
+'use strict';
+const common = require('../../common'),
+      expect = require('chai').expect;
+
+function loginAndClickTemplates() {
+  return common.login(browser, 'admin', 'admin', 'local')
+      .then(() => common.clickSidebarTab(browser, 'Templates'))
+}
+
+function somehowClearThePathField() { return Promise.resolve(); }
+
+describe('Templates (Validations)', () => {
+  beforeEach(() => loginAndClickTemplates());
+  it('should refuse invalid template paths', () => {
+    let form = '//form[@id="newTmplForm"]';
+
+    return browser.click('//button[@id="new_tmpl"]')
+      .then(() => browser.setValue(`${form}//input[@id="path"]`, 'invalid input'))
+      .then(() => browser.click(`${form}//input[@name="name"]`))
+      //.then(() => browser.waitForExist('//span[@id=path-message]'))
+      .then(() => browser.getText('//span[@id="path-message"]'))
+      .then((text) => expect(text).to.equal("Path verification failed - can't mount destination."))
+  });
+
+  // it('puts', () => {
+  //   expect(true).to.be.true;
+  // });
+
+  // it('the lotion', () => {
+  //   expect(true).to.be.true;
+  // });
+
+  // it('on', () => {
+  //   expect(true).to.be.true;
+  // });
+
+
+  // describe('Path', () => {
+  //   afterEach(() => somehowClearThePathField());
+  //   it('should not allow the word monkey', () => {
+  //     expect(true).to.be.true;
+  //   });
+
+  //   it('should not allow the characters $%^^&**^%', () => {
+  //     expect(true).to.be.false;
+  //   });
+  // })
+
+});
+
+
