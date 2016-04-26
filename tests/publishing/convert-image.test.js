58c5593150d4b7a7d0db2ff0194b5c04999846ad
Olek <malahoff@gmail.com>
2016-04-26 14:55:08 -0400

    feat(convert image validation): check that convert image page does not validate empty source input

--- tests/publishing/convert-image.test.js
+++ tests/publishing/convert-image.test.js
@@ -5,4 +5,11 @@ describe('Convert Image', () => {
   before(() => common.login(browser, 'admin', 'admin', 'local'));
   it('should navigate to Convert Image', () =>
     common.clickSidebarTab(browser, 'Convert Image'));
+
+  it('should validate empty source input', () => {
+  	return browser.getValue('//input[@id="source"]')
+  	  .then((text) => expect(text).to.equal(''))
+  	  .then(() => browser.click('//button[@id="btn_convert"]'))
+  	  .then(() => browser.isVisible('//div[@class="has-error"]'))
+  });
 });
