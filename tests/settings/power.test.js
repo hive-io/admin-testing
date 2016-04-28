b06e913213881ef510da9fcc6704f85b99a5301f
Olek <malahoff@gmail.com>
2016-04-28 10:25:54 -0400

    fix(power): Removed power file because it is no longer a section on the site

--- tests/settings/power.test.js
+++
@@ -1,8 +0,0 @@
-const common = require('../common'),
-      expect = require('chai').expect;
-
-describe('Power', () => {
-  before(() => common.login(browser, 'admin', 'admin', 'local'));
-  it('should navigate to Power', () =>
-    common.clickSidebarTab(browser, 'Power', 'Power Options'));
-});
