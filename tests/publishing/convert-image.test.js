94afcc400d379015da7c63193c8e8928aec76b9a
Olek <malahoff@gmail.com>
2016-04-28 10:20:39 -0400

    feat(convert-img): Check if error message source
    Check Convert Image source input displays error message with
    invalid or empty input

--- tests/publishing/convert-image.test.js
+++ tests/publishing/convert-image.test.js
@@ -2,14 +2,22 @@ const common = require('../common'),
       expect = require('chai').expect;
 
 describe('Convert Image', () => {
-  before(() => common.login(browser, 'admin', 'admin', 'local'));
-  it('should navigate to Convert Image', () =>
-    common.clickSidebarTab(browser, 'Convert Image'));
+  beforeEach(() => common.login(browser, 'admin', 'admin', 'local')
+    .then(() => common.clickSidebarTab(browser, 'Convert Image'))
+  );
 
-  it('should validate empty source input', () => {
+  // it('should navigate to Convert Image', () =>
+  //   common.clickSidebarTab(browser, 'Convert Image'));
+
+  it('should complain about empty source input', () => {
   	return browser.getValue('//input[@id="source"]')
   	  .then((text) => expect(text).to.equal(''))
   	  .then(() => browser.click('//button[@id="btn_convert"]'))
   	  .then(() => browser.isVisible('//div[@class="has-error"]'))
   });
+  it('should complain about invalid source input', () => {
+    return browser.setValue('//input[@id="source"]', 'invalid input')
+      .then(() => browser.click('//button[@id="btn_convert"]'))
+      .then(() => browser.isVisible('//div[@class="has-error"]'))
+  });  
 });
