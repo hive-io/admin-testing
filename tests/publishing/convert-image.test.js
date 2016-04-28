746ec47b126cc8c818fae964a8537d1ad0bcce89
Olek <malahoff@gmail.com>
2016-04-28 13:12:27 -0400

    feat(convert image): check if error messages are cleared
    Check if source and output error messages are cleared after
    multiple erroneous inputs

--- tests/publishing/convert-image.test.js
+++ tests/publishing/convert-image.test.js
@@ -6,18 +6,32 @@ describe('Convert Image', () => {
     .then(() => common.clickSidebarTab(browser, 'Convert Image'))
   );
 
-  // it('should navigate to Convert Image', () =>
-  //   common.clickSidebarTab(browser, 'Convert Image'));
-
   it('should complain about empty source input', () => {
   	return browser.getValue('//input[@id="source"]')
   	  .then((text) => expect(text).to.equal(''))
   	  .then(() => browser.click('//button[@id="btn_convert"]'))
   	  .then(() => browser.isVisible('//div[@class="has-error"]'))
   });
+  
   it('should complain about invalid source input', () => {
     return browser.setValue('//input[@id="source"]', 'invalid input')
       .then(() => browser.click('//button[@id="btn_convert"]'))
       .then(() => browser.isVisible('//div[@class="has-error"]'))
-  });  
+  });
+
+  it('should clear old source error messages', () => {
+    return browser.setValue('//input[@id="source"]', 'invalid input')
+      .then(() => browser.click('//button[@id="btn_convert"]'))
+      .then(() => browser.setValue('//input[@id="source"]', ''))
+      .then(() => browser.click('//button[@id="btn_convert"]'))
+      .then(() => browser.waitForExist('//input[@id="source"]/..//i[position()=2])',500,true))
+  });
+
+  it('should clear old output error messages', () => {
+    return browser.setValue('//input[@id="output"]', 'invalid input')
+      .then(() => browser.click('//button[@id="btn_convert"]'))
+      .then(() => browser.setValue('//input[@id="output"]', ''))
+      .then(() => browser.click('//button[@id="btn_convert"]'))
+      .then(() => browser.waitForExist('//input[@id="output"]/..//i[position()=2])',500,true))
+  });      
 });
