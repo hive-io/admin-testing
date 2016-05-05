be5e4f034999c461fdc2731f78df2cd7b5b5094d
Olek <malahoff@gmail.com>
2016-05-05 11:57:03 -0400

    fix(new_users): longer wait time

--- tests/administration/users/new_user_validation.test.js
+++ tests/administration/users/new_user_validation.test.js
@@ -9,21 +9,13 @@ describe('New User Validations', () => {
 
   it('should not allow empty name', () => {
   	return common.createNewUser('', 'local', 'readonly', 'admin')
-      .then(() => console.log("GOT HERE"))
-      .then(() => browser.waitForExist('//td[not(string()) and position()=1]', 500, true))
-      .then(() => console.log("GOT HERE 2"));
+      .then(() => browser.waitForExist('//td[not(string()) and position()=1]', 5000, true))
   });
 
   it('should not allow an empty password', () => {
   	return common.createNewUser('test_user', 'local', 'readonly', '')
-      .then(() => browser.waitForExist('//td[text()="test_user" and position()=1]', 500, true))
+      .then(() => browser.waitForExist('//td[text()="test_user" and position()=1]', 5000, true))
 
   });
 
-  it('should clean up', () => {
-    return browser.isExisting('//td[not(string()) and position()=1]')
-  	  .then((ex) => ex ? browser.click('//td[not(string()) and position()=1]/..//button[contains(@id,"rm")]') : null)
-  	  .then(() => browser.isExisting('//td[text()="test_user" and position()=1]'))
-  	  .then((ex) => ex ? browser.click('//td[text()="test_user" and position()=1]/..//button[contains(@id,"rm")]') : null)
-  });
 });
