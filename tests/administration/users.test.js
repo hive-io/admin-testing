9e3334ddb14915285acab21546ad31caa0cf234e
Olek <malahoff@gmail.com>
2016-05-05 11:52:51 -0400

    fix(users): fixed code to reflect changes made on the site as well as using a new way to check that button onclick is not null

--- tests/administration/users.test.js
+++ tests/administration/users.test.js
@@ -12,21 +12,29 @@ describe('Users', () => {
   afterEach(() => common.logout());
   
   it('should allow password changes for admin', () => {
-  	return browser.click('//td[contains(text(),"admin") and position()=1]/..//button[contains(@id,"cp")]')
-  	  .then(() => browser.waitForExist('//td[contains(text(),"admin") and position()=1]/..//form//input[@id="password"]'))
-      .then(() => browser.setValue('//td[contains(text(),"admin") and position()=1]/..//form//input[@id="password"]', 'admin'))
-  	  .then(() => browser.click('//td[contains(text(),"admin") and position()=1]/..//form//button[@type="submit"]'))
+  	return browser.waitUntil(
+        browser.getAttribute('//td[text()="admin" and position()=1]/..//button[contains(@id,"cp")]', 'onclick')
+          .then((cl) => { return cl !== null; })
+      ,10000)
+      .then(() => browser.click('//td[text()="admin" and position()=1]/..//button[contains(@id,"cp")]'))
+  	  .then(() => browser.waitForExist('//td[text()="admin" and position()=1]/..//form//input[@id="password"]'))
+      .then(() => browser.setValue('//td[text()="admin" and position()=1]/..//form//input[@id="password"]', 'admin'))
+      .then(() => browser.waitUntil(
+        browser.getAttribute('//td[text()="admin" and position()=1]/..//form//button[@type="submit"]', 'type')
+          .then((cl) => { return cl !== null; })
+      ,10000))
+  	  .then(() => browser.click('//td[text()="admin" and position()=1]/..//form//button[@type="submit"]'))
       .then(() => browser.waitForExist('//h1[@class="page-header"]'))
       .then(() => browser.getText('//h1[@class="page-header"]'))
   	  .then((text) => expect(text).to.equal('System Users'));
   });
 
   it('should allow password changes for readonly user', () => {
-  	return browser.waitForExist('//td[text()="readonly" and position()=1]/..//button[contains(@id,"cp")]')
+  	return browser.waitForExist('//tbody')
       .then(() => browser.isExisting('//td[text()="readonly" and position()=1]/..//button[contains(@id,"cp")]'))
       .then((exists) => !exists ? common.createNewUser('readonly', 'local', 'readonly', 'admin') : null )
-  	  .then(() => browser.waitForExist('//td[text()="readonly" and position()=1]/..//button[contains(@id,"cp")]', 1000))
-      .then(() => browser.click('//td[text()="readonly" and position()=1]/..//button[contains(@id,"cp")]'))
+  	  .then(() => browser.waitForExist('//td[text()="readonly" and position()=1]/..//button[contains(@id,"cp")]', 5000))
+      .then(() => common.waitAndClick('//td[text()="readonly" and position()=1]/..//button[contains(@id,"cp")]'))
   	  .then(() => browser.setValue('//td[text()="readonly" and position()=1]/..//input[@id="password"]', 'admin'))
   	  .then(() => browser.click('//td[text()="readonly" and position()=1]/..//button[@type="submit"]'))
   	  .then(() => browser.waitForExist('//h1[@class="page-header"]')) 
@@ -35,9 +43,8 @@ describe('Users', () => {
   });
 
   it('should allow deletion of readonly user', () => {
-  	return browser.waitForExist('//td[text()="readonly" and position()=1]/..//button[contains(@id,"rm")]')
-      .then(() => browser.click('//td[text()="readonly" and position()=1]/..//button[contains(@id,"rm")]'))
-  	  .then(() => browser.waitForExist('//td[text()="readonly" and position()=1]/..//button[contains(@id,"rm")]', 500, true))
+  	return common.waitAndClick('//td[text()="readonly" and position()=1]/..//button[contains(@id,"rm")]')
+  	  .then(() => browser.waitForExist('//td[text()="readonly" and position()=1]/..//button[contains(@id,"rm")]', 5000, true))
   });
 
   it('should allow creation of a new readonly user', () => {
@@ -48,8 +55,12 @@ describe('Users', () => {
   it('should allow for creation and deletion of new admin user', () => {
     return common.createNewUser('test','local','admin','admin')
       .then(() => browser.waitForExist('//td[text()="test" and position()=1]/..//button[contains(@id,"rm")]'))
+      .then(() => browser.waitUntil(
+        browser.getAttribute('//td[text()="test" and position()=1]/..//button[contains(@id,"rm")]', 'onclick')
+          .then((cl) => { return cl !== null; })
+        ,10000))
       .then(() => browser.click('//td[text()="test" and position()=1]/..//button[contains(@id,"rm")]'))
-      .then(() => browser.waitForExist('//td[text()="test" and position()=1]/..//button[contains(@id,"rm")]', 500, true))
+      .then(() => browser.waitForExist('//td[text()="test" and position()=1]/..//button[contains(@id,"rm")]', 5000, true))
   });
 
   it('should not overwrite existing users', () => {
@@ -59,12 +70,12 @@ describe('Users', () => {
       .then(() => common.createNewUser(login.name, login.realm, login.role, login.wrongpass))
       .then(() => common.logout())
       .then(() => common.login(browser, login.name, login.wrongpass, 'local' ))
-      .then(() => browser.waitForExist('//div[contains(@class,"errormessage")]',500))
+      .then(() => browser.waitForExist('//div[contains(@class,"errormessage")]',5000))
       .then(() => common.login(browser, login.name, login.pass, 'local'))
       .then(() => common.clickSidebarTab(browser, 'Users', 'System Users'));
   });
   
   it('should not allow deletion of current admin user', () => {
-    return browser.waitForExist(`//td[text()="admin" and position()=1]/..//button[contains(@id,"rm")]`, 500, true)
+    return browser.waitForExist(`//td[text()="admin" and position()=1]/..//button[contains(@id,"rm")]`, 5000, true)
   });
 });
