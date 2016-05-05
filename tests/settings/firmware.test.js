7676d9affd4edd36de408acaa016944873742b1e
Olek <malahoff@gmail.com>
2016-05-05 13:03:09 -0400

    test(firmware): invalid submission test

--- tests/settings/firmware.test.js
+++ tests/settings/firmware.test.js
@@ -7,4 +7,15 @@ describe('Firmware', () => {
   after(() => common.logout());
   it('should navigate to Firmware', () =>
     common.clickSidebarTab(browser, 'Firmware', 'Firmware Management'));
+
+  it('should stay on page after invalid selection', () => {
+  	let myURL;
+  	return browser.getUrl()
+  	  .then((url) => myURL = url )
+  	  .then(() => browser.waitForEnabled('//button[@type="submit"]', 10000))
+  	  .then(() => browser.click('//button[@type="submit"]'))
+  	  .then(() => browser.getUrl())
+  	  .then((url) => expect(url).to.equal(myURL));
+  });
+  
 });
