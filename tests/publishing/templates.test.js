2d5fe35e99cc0506d9923511737fbae8e4dc84f8
Olek <malahoff@gmail.com>
2016-05-05 12:24:35 -0400

    test(templates): basic ui testing for template creatyion and addition

--- tests/publishing/templates.test.js
+++ tests/publishing/templates.test.js
@@ -8,19 +8,30 @@ describe('Templates', () => {
   it('should navigate to Templates', () => common.clickSidebarTab(browser, 'Templates'));
 
   it('should allow user to create a new template', () => {
-    let realHeader = '//div[@id="editTemplate"]//h1[@class="page-header"]';
-    return browser.click('//button[@id="new_tmpl"]')
-      .then(() => browser.waitForExist(realHeader))
-      .then(() => browser.getText(realHeader))
+    return common.waitForOnClick('//button[@id="new_tmpl"]')
+      .then(() => browser.click('//button[@id="new_tmpl"]'))
+      .then(() => browser.waitForExist('//div[@id="editTemplate"]//h1[@class="page-header"]'))
+      .then(() => browser.getText('//div[@id="editTemplate"]//h1[@class="page-header"]'))
       .then(title => expect(title).to.equal('Create New Template'));
   });
 
-  it('should fill in the new template data', () => {
-    let name = '//form[@id="newTmplForm"]//input[@name="name"]';
-    return browser.waitForEnabled(name)
-      .then(() => browser.setValue(name, 'Windows 10.1 x64 RE'))
-      .then(() => browser.getValue(name))
-      .then((text) => expect(text).to.equal('Windows 10.1 x64 RE'));
+  it('should allow user to cancel creating a new template', () => {
+    return common.waitForOnClick('//*[@id="newTmplForm"]/div[11]/button[2 and text()="Cancel"]')
+      .then(() => browser.click('//*[@id="newTmplForm"]/div[11]/button[2 and text()="Cancel"]'))
+      .then(() => browser.waitForExist('//*[@id="newTmplForm"]', 1000, true));
+  });
+
+  it('should allow user to add a template', () => {
+    return browser.waitForExist('//*[@id="tmpl_form" and @class="hidden"]')
+      .then(() => common.waitForOnClick('//*[@id="add_tmpl"]'))
+      .then(() => browser.click('//*[@id="add_tmpl"]'))
+      .then(() => browser.waitForExist('//*[@id="add_tmpl_form"]'))
+  });
+
+  it('should allow user to cancel adding a template', () => {
+    return common.waitForOnClick('//*[@id="add_tmpl_form"]/div[3]/button[2 and text()="Cancel"]')
+      .then(() => browser.click('//*[@id="add_tmpl_form"]/div[3]/button[2 and text()="Cancel"]'))
+      .then(() => browser.waitForExist('//*[@id="tmpl_form" and @class="hidden"]'));
   });
 
 });
