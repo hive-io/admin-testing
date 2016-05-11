4e8c0e3e15d56ff974dd37c844c7f236163dcbaa
Olek <malahoff@gmail.com>
2016-05-11 11:08:03 -0400

    feat(login): commented out login tests as they are duplicative and unnecessary as well as redundant

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,22 +1,24 @@
-const common = require('./common'),
-      expect = require('chai').expect;
+//This test is in common and part of every other test.
 
-describe('log in to the admin page', () => {
-  it('should load the login page', () => {
-    return browser.url('/').getTitle()
-      .then(title => expect(title).to.startWith('hiveIO Appliance |'));
-  });
+// const common = require('./common'),
+//       expect = require('chai').expect;
 
-  it('should login', () => {
-    return browser.setValue('//input[@name="username"]', 'admin')
-      .then(() => browser.setValue('//input[@name="password"]', 'admin'))
-      .then(() => browser.selectByVisibleText('//select[@name="domain"]', 'local'))
-      .then(() => browser.click('//button[@type="submit"]'));
-  });
+// describe('log in to the admin page', () => {
+//   it('should load the login page', () => {
+//     return browser.url('/').getTitle()
+//       .then(title => expect(title).to.startWith('hiveIO Appliance |'));
+//   });
 
-  it('should then be on the main dashboard page', () => {
-    return browser.waitForExist('h1.page-header')
-      .then(() => browser.getText('h1.page-header'))
-      .then(header => expect(header).to.equal('Overview'));
-  });
-});
+//   it('should login', () => {
+//     return browser.setValue('//input[@name="username"]', 'admin')
+//       .then(() => browser.setValue('//input[@name="password"]', 'admin'))
+//       .then(() => browser.selectByVisibleText('//select[@name="domain"]', 'local'))
+//       .then(() => browser.click('//button[@type="submit"]'));
+//   });
+
+//   it('should then be on the main dashboard page', () => {
+//     return browser.waitForExist('h1.page-header')
+//       .then(() => browser.getText('h1.page-header'))
+//       .then(header => expect(header).to.equal('Overview'));
+//   });
+// });
