4664990c575f8100fe1c01edf8a22fbfa5ac2118
Olek <malahoff@gmail.com>
2016-09-16 16:42:53 -0400

    chore: basic configuration

--- testconfig.js
+++ testconfig.js
@@ -1,28 +1,30 @@
 'use strict';
 
 let server = !!process.env.SERVER ?
-  'http://' + process.env.SERVER : 'http://192.168.11.88';
+  'https://' + process.env.SERVER : 'https://192.168.1.200';
 
 let specs = !!process.env.TEST ?
 [process.env.TEST] :
 [
   //'./tests/*.test.js',
-  './tests/**/*.test.js',
-  './tests/**/**/*.test.js'
+  './tests/scenarios/*.test.js'
+  // './tests/**/**/*.test.js'
 ];
 
-let nfsIP = !!process.env.NFS ? process.env.NFS : '192.168.11.4';
+let nfsIP = !!process.env.NFS ? process.env.NFS : '192.168.1.5';
 
-let tmplPath = !!process.env.TMPLPATH ? process.env.TMPLPATH : '/Guests';
+let tmplPath = !!process.env.TMPLPATH ? process.env.TMPLPATH : '/templates';
 
-let isoPath = !!process.env.ISOPATH ? process.env.ISOPATH : '/Iso';
+let isoPath = !!process.env.ISOPATH ? process.env.ISOPATH : '/vms';
 
-let nfsPath = !!process.env.NFSPATH ? process.env.NFSPATH : '/NFS';
+let nfsPath = !!process.env.NFSPATH ? process.env.NFSPATH : '/storage';
 
 let exclude = !!process.env.EXCLUDE ?
 [process.env.EXCLUDE] :
 [
-  // './tests/scenarios/convert-template.test.js',
+  // './tests/scenarios/*'
+  // './tests/administration/users/*',
+  // './tests/scenarios/convert-template.test.js'
   // './tests/scenarios/windows-with-realm.test.js'
   // './tests/scenarios/ou.test.js',
   // './tests/scenarios/realm-validation.test.js'
@@ -41,3 +43,21 @@ module.exports = {
   exclude: exclude,
   browser: browser
 };
+
+
+//OFER TEST CONFIG
+
+/*
+let server = !!process.env.SERVER ?
+  'http://' + process.env.SERVER : 'http://192.168.1.200';
+
+let nfsIP = !!process.env.NFS ? process.env.NFS : '192.168.1.5';
+
+let tmplPath = !!process.env.TMPLPATH ? process.env.TMPLPATH : '/templates';
+
+let isoPath = !!process.env.ISOPATH ? process.env.ISOPATH : '/vms';
+
+let nfsPath = !!process.env.NFSPATH ? process.env.NFSPATH : '/storage';
+
+*/
+//
